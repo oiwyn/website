99b873e130acaeff77e6fb7ff4b81b8285a1d659
oiwyn <owynn@outlook.sg>
2021-02-04 14:29:25 +0800

    added uranime to project

--- src/components/work/workData.js
+++ src/components/work/workData.js
@@ -1,5 +1,28 @@
 export const workContent = [
     {
+        name: "URANIME",
+        id: "uranime",
+        filter: "personal",
+        image: require("./uranime/cover.png").default,
+        desc: {
+            type: "App UI/UX Design",
+            client: "Personal Project (2020)",
+            tools: "Figma",
+            content: "URANIME is a personal project and my personal take on what a native anime streaming Android app would look like.\n\nIn practice, the app will outsources shows detail - both series and movies, from MyAnimeList, an anime and manga database website that will enable functions such as bookmarking, following, discovery and community recommendations.\nIt will also allow users to choose their desired streaming server for each anime from the already existing fan supported streaming servicecs.\n\nThe design is currently unfinished and the live prototype you see below is a work-in-progress.",
+        },
+        misc:{
+            live: false,
+            figma:{
+                value: true,
+                link: "https://www.figma.com/proto/2DtKBRlrGBS6sH0d300YXg/URANIME?node-id=1%3A2&scaling=scale-down-width",   
+            },
+            image:{
+                cover: require("./uranime/header.png").default,
+                content: require("./uranime/content.png").default,
+            }
+        } 
+    },
+    {
         name: "Express Bahari",
         id: "express-bahari",
         filter: "professional",
@@ -8,7 +31,7 @@ export const workContent = [
             type: "Website and App UI/UX Design",
             client: "PT. Pelayaran Sakti Inti Makmur (2019)",
             tools: "Figma, Adobe Xd, Adobe Photoshop, HTML, CSS, JavaScript",
-            content: "Express Bahari, one of the biggest ferry service in Indonesia approached me to design their website and their upcoming app.\n\nThey wanted to upgrade its service by building a responsive website that has location-based ferry timetable, promo selections, and most importantly it has to be user friendly no matter on mobile, tablet, or PC. Following the redesign, the company liked my design direction and asked me to do UI/UX design for their upcoming smartphone app that has features, such as allowing users to book and see their trip detail, redeem promo codes, push notification, and rewards system",
+            content: "Express Bahari, one of the biggest ferry service in Indonesia approached me to design their website and their upcoming app.\n\nThey wanted to upgrade its service by building a responsive website that has location-based ferry timetable, promo selections, and most importantly it has to be user friendly no matter on mobile, tablet, or PC. Following the redesign, the company liked my design direction and asked me to do UI/UX design for their upcoming smartphone app that has features, such as allowing users to book and see their trip detail, redeem promo codes, push notification, and rewards system.",
         },
         misc:{
             live: "https://oiwyn.github.io/express-bahari/",
@@ -51,8 +74,8 @@ export const workContent = [
         image: require("./pronoviastw/cover.png").default,
         desc: {
             type: "Website Design",
-            client: "ClubMax Studio (2020)",
-            tools: "CSS, WordPress",
+            client: "ClubMax Studio (2019)",
+            tools: "Wix",
             content: "My position at Club Max, a wedding oriented company located at Taipei and one of the Pronovias dresses distributor in Taiwan, required me to design a website that allows our clients to browse dresses and book appointment online.\n\nSince our time was limited, the management and the sales teams decided to build the website on Wix as they found Wix had the easiest system for them to manage.\nAs Wix's features were limited, my work was mostly on designing the website layout, daily troubleshooting, and weekly dresses database management."
         },
         misc:{

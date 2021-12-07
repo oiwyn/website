f1924d2d17f260d420eababfb3b8c06c3a6161de
Owynn <46760876+oiwyn@users.noreply.github.com>
2021-12-07 21:14:41 +0700

    cv -> resume

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ export default function App() {
                     </Link>
                     <Link to="/resume">
                         <li>
-                            CV
+                            Resume
                         </li>
                     </Link>
                 </ul>

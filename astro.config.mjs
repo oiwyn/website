55f2eaca7d202d21ef816bc52f461781a5aeb77a
oiwyn <kenliwynn@gmail.com>
2026-05-23 21:44:31 +0700

    loading icon tweak test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vtbot from 'astro-vtbot';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     watch: ['src/content/**/*.md']
   },
+
+  integrations: [vtbot()],
 });
